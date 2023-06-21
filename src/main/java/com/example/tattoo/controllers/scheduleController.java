1e1053d143d11628e2b9d64cec69366e9955141f
tebyarce <earce1999@gmail.com>
2023-06-21 15:49:56 -0600

    update schedule

--- src/main/java/com/example/tattoo/controllers/scheduleController.java
+++ src/main/java/com/example/tattoo/controllers/scheduleController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,9 +22,20 @@ public class scheduleController {
     @Autowired
     scheduleService scheduleService;
 
-    @GetMapping( path = "/{id}")
+    @GetMapping()
+    public ArrayList<schedule> getSchedules() {
+        return scheduleService.getSchedules();
+    }
+
+    @GetMapping( path = "/user/{id}")
     public ArrayList<schedule> getSchedulesByUser(@PathVariable("id") String id){
         return this.scheduleService.getSchedulesbyUser(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSchedule(@PathVariable("id") Integer id) {
+        Optional<schedule> schedule = scheduleService.getById(id).get();
+        schedule.ifPresent(scheduleService::removeSchedule);
     }
 
     @PostMapping()
